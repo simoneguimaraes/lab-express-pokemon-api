79426b2d05ab8dce42bddc6c3fc9f6c55af3af2a
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-29 19:33:53 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-
+const app = express();
 const PORT = 4000;
+
+// Hint: Don't forget that to be able to receive data from the client, you have to tell Express to accept JSON request bodies :)
+const postRouter = require("./posts");
+app.use(express.json());
+app.use("/", postRouter);
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
 
-const app = express();
 
 // -- Define your route listeners here! --
-
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
